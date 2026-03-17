d92fb0e49a7b6e708843eb26f857d3f128b4643f
Izzeddine Bouzid <ibouzid.c@stc.com.sa>
2026-03-17 14:17:34 +0300

    Adds policy to allow prud url

--- WordQuest.Api/Program.cs
+++ WordQuest.Api/Program.cs
@@ -5,22 +5,31 @@ builder.Services.AddHttpClient<WordService>(client =>
 {
     client.Timeout = TimeSpan.FromSeconds(10);
 });
+
 builder.Services.AddSingleton<GameService>();
+const string myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend",
+    options.AddPolicy(myAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod()
-                  .AllowCredentials();
+            policy.WithOrigins(
+                "http://localhost:5173", 
+                "https://word-quest-ibouzids-projects.vercel.app"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
         });
 });
+
 builder.Services.AddSignalR();
 
 var app = builder.Build();
-app.UseCors("AllowFrontend");
+app.UseCors(myAllowSpecificOrigins);
+app.UseRouting(); 
 app.MapControllers();
 app.MapHub<GameHub>("/gamehub");
+
 app.Run();
